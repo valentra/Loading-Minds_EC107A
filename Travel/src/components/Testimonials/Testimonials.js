85b2b6cfcc1d4f1eb91ae19e671c33ea508486fc
Anushka Bhowmick <76967222+Anushka-Bhowmick@users.noreply.github.com>
2024-03-04 02:36:12 +0530

    testimonials added

--- Travel/src/components/Testimonials/Testimonials.js
+++ Travel/src/components/Testimonials/Testimonials.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
+import { MDBCard, MDBCardBody, MDBCol, MDBContainer, MDBRow } from "mdb-react-ui-kit";
+import "./Testimonials.css"; // Import the CSS file for styling
 
-const Testimonials = () => {
+export default function Testimonials() {
   return (
-    <div>
-      
+    <div className="background-container">
+      <div className="card-container">
+        <MDBContainer fluid>
+          <MDBRow className="d-flex justify-content-center">
+            <MDBCol md="8" lg="6" xl="4">
+              <MDBCard>
+                <MDBCardBody className="m-3">
+                  <MDBRow className="align-items-center">
+                    <MDBCol lg="4" className="d-flex justify-content-center">
+                      <img
+                        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20%2810%29.webp"
+                        alt="woman avatar"
+                        className="circular-image"
+                      />
+                    </MDBCol>
+                    <MDBCol lg="8">
+                      <p className="text-muted fw-light mb-4">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Id quam sapiente molestiae numquam quas, voluptates
+                        omnis nulla ea odio quia similique corrupti magnam.
+                      </p>
+                      <p className="fw-bold lead mb-2">
+                        <strong>Anna Smith</strong>
+                      </p>
+                      <p className="fw-bold text-muted mb-0">Product manager</p>
+                    </MDBCol>
+                  </MDBRow>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </div>
     </div>
-  )
+  );
 }
-
-export default Testimonials
