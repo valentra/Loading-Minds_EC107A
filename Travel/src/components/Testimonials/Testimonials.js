05dee8c42b74a744e9dd0b6037aeb178213d3663
Indrani Som <indranisom39@gmail.com>
2024-03-03 18:36:56 -0800

    added:picture added

--- Travel/src/components/Testimonials/Testimonials.js
+++ Travel/src/components/Testimonials/Testimonials.js
@@ -1,42 +1,53 @@
-import React from "react";
-import { MDBCard, MDBCardBody, MDBCol, MDBContainer, MDBRow } from "mdb-react-ui-kit";
-import "./Testimonials.css"; // Import the CSS file for styling
+import React from 'react'
 
-export default function Testimonials() {
+const Testimonials = () => {
   return (
-    <div className="background-container">
-      <div className="card-container">
-        <MDBContainer fluid>
-          <MDBRow className="d-flex justify-content-center">
-            <MDBCol md="8" lg="6" xl="4">
-              <MDBCard>
-                <MDBCardBody className="m-3">
-                  <MDBRow className="align-items-center">
-                    <MDBCol lg="4" className="d-flex justify-content-center">
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20%2810%29.webp"
-                        alt="woman avatar"
-                        className="circular-image"
-                      />
-                    </MDBCol>
-                    <MDBCol lg="8">
-                      <p className="text-muted fw-light mb-4">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. Id quam sapiente molestiae numquam quas, voluptates
-                        omnis nulla ea odio quia similique corrupti magnam.
-                      </p>
-                      <p className="fw-bold lead mb-2">
-                        <strong>Anna Smith</strong>
-                      </p>
-                      <p className="fw-bold text-muted mb-0">Product manager</p>
-                    </MDBCol>
-                  </MDBRow>
-                </MDBCardBody>
-              </MDBCard>
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
-      </div>
+    <div>
+      
     </div>
-  );
+  )
 }
+
+export default Testimonials
+// import React from "react";
+// import { MDBCard, MDBCardBody, MDBCol, MDBContainer, MDBRow } from "mdb-react-ui-kit";
+// import "./Testimonials.css"; // Import the CSS file for styling
+
+// export default function Testimonials() {
+//   return (
+//     <div>
+//     <h2>Automatic Slideshow</h2>
+// <p>Change image every 2 seconds:</p>
+
+// <div class="slideshow-container">
+
+// <div class="mySlides fade">
+//   <div class="numbertext">1 / 3</div>
+//   <img src="img_nature_wide.jpg" style="width:100%">
+//   <div class="text">Caption Text</div>
+// </div>
+
+// <div class="mySlides fade">
+//   <div class="numbertext">2 / 3</div>
+//   <img src="img_snow_wide.jpg" style="width:100%">
+//   <div class="text">Caption Two</div>
+// </div>
+
+// <div class="mySlides fade">
+//   <div class="numbertext">3 / 3</div>
+//   <img src="img_mountains_wide.jpg" style="width:100%">
+//   <div class="text">Caption Three</div>
+// </div>
+
+// </div>
+// <br/>
+
+// <div style="text-align:center">
+//   <span class="dot"></span> 
+//   <span class="dot"></span> 
+//   <span class="dot"></span> 
+// </div>
+
+//     </div>
+//   );
+// }
